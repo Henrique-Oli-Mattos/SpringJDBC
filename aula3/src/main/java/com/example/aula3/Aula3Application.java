ccac211c9b7bb18507f3f7a7084c0ce1388c6c66
Henrique Oliveira <64371355+Henrique-Oli-Mattos@users.noreply.github.com>
2022-04-03 20:21:12 -0300

    Add files via upload

---
+++ aula3/src/main/java/com/example/aula3/Aula3Application.java
@@ -0,0 +1,106 @@
+package com.example.aula3;
+
+import java.util.List;
+
+import com.example.aula3.entity.Usuario;
+import com.example.aula3.repository.UsuarioRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Aula3Application {
+
+	//Inserir usuario:
+
+	// @Bean
+	// public CommandLineRunner 
+	// init (@Autowired UsuarioRepository usuarioRepository){
+	// 	return args ->{
+	// 		usuarioRepository.inserir(
+	// 			new Usuario(0, "teste", "teste@teste", "123"));
+	// 		List<Usuario> listaUsuarios = usuarioRepository.obterTodos();
+	// 		listaUsuarios.forEach(System.out::println);
+	// 	};
+	// }
+
+	// Remover (int id):
+
+	// @Bean
+	// public CommandLineRunner 
+	// init (@Autowired UsuarioRepository usuarioRepository){
+	// 	return args ->{
+	// 		usuarioRepository.remover(2);
+	// 		List<Usuario> listaUsuarios = usuarioRepository.obterTodos();
+	// 		listaUsuarios.forEach(System.out::println);
+	// 	};
+	// }
+
+	//Remover (Usuario usuario)
+
+	// @Bean
+	// public CommandLineRunner 
+	// init (@Autowired UsuarioRepository usuarioRepository){
+	// 	return args ->{
+
+	// 		Usuario usuario = new Usuario(2, "teste3", "teste@teste", "123");
+	// 		usuarioRepository.inserir(usuario);
+
+	// 		List<Usuario> listaUsuarios = usuarioRepository.obterTodos();
+	// 		listaUsuarios.forEach(System.out::println);
+
+	// 		usuarioRepository.remover(usuario);
+
+	// 		List<Usuario> listaUsuarios2 = usuarioRepository.obterTodos();
+	// 		listaUsuarios2.forEach(System.out::println);
+
+	// 	};
+	// }
+
+	//editar:
+
+	// @Bean
+	// public CommandLineRunner 
+	// init (@Autowired UsuarioRepository usuarioRepository){
+	// 	return args ->{
+
+	// 		Usuario usuario = new Usuario(2, "teste3", "teste@teste", "123");
+	// 		usuarioRepository.inserir(usuario);
+
+	// 		List<Usuario> listaUsuarios = usuarioRepository.obterTodos();
+	// 		listaUsuarios.forEach(System.out::println);
+
+	// 		Usuario usuario2 = new Usuario(2, "EDITADO", "Editado@teste", "123");
+	// 		usuarioRepository.editar(usuario2);
+
+	// 		List<Usuario> listaUsuarios2 = usuarioRepository.obterTodos();
+	// 		listaUsuarios2.forEach(System.out::println);
+
+	// 	};
+	// }
+
+	@Bean
+	public CommandLineRunner 
+	init (@Autowired UsuarioRepository usuarioRepository){
+		return args ->{
+
+			Usuario usuario = new Usuario(2, "teste3", "teste@teste", "123");
+			usuarioRepository.inserir(usuario);
+
+			usuarioRepository.autenticar("teste@teste", "123");
+
+			List<Usuario> listaUsuarios = usuarioRepository.obterTodos();
+			listaUsuarios.forEach(System.out::println);
+
+
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula3Application.class, args);
+	}
+
+}
